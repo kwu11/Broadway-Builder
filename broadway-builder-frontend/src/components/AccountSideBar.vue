12c667c656d0219537a42ed2eeccd81cab4be97f
zanderlx <lsaplan97@gmail.com>
2019-04-25 12:46:55 -0700

    Added a sidebar for admin dashboard

--- broadway-builder-frontend/src/components/AccountSideBar.vue
+++ broadway-builder-frontend/src/components/AccountSideBar.vue
@@ -32,11 +32,8 @@
               <v-list-tile-title>User Management</v-list-tile-title>
             </v-list-tile>
           </template>
-          <v-list-tile v-for="(crud, i) in cruds" :key="i" @click="">
+          <v-list-tile v-for="(crud, i) in cruds" :key="i">
             <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
-            <v-list-tile-action>
-              <v-icon v-text="crud[1]"></v-icon>
-            </v-list-tile-action>
           </v-list-tile>
         </v-list-group>
       </v-list-group>
