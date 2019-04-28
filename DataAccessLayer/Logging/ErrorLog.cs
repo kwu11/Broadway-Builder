5019380d2bfd7d44a3db69bc6ec7968ed0ec5894
DeletedSilence <31982323+DeletedSilence@users.noreply.github.com>
2019-04-28 00:19:37 -0700

    created MongoDbContext and started Errorlog class

---
+++ DataAccessLayer/Logging/ErrorLog.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Logging
+{
+    public class ErrorLog
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ID { get; set; }
+
+        [BsonElement("User")]
+        public int userId { get; set; }
+
+        [BsonElement("Request")]
+        public string request { get; set; }
+
+        [BsonElement("Message")]
+        public string message { get; set; }
+
+        [BsonElement("Layer")]
+        public string layer { get; set; }
+
+        [BsonElement("TimeStamp")]
+        public DateTime timeStamp{get;set;}
+
+    }
+}
