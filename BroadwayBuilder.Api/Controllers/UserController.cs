ff24e43cb29f0c2dea2874c6c6c44630ef10e122
ohfoeshow <36217477+ohfoeshow@users.noreply.github.com>
2019-05-01 19:51:01 -0700

    Create User frontend and controller

--- BroadwayBuilder.Api/Controllers/UserController.cs
+++ BroadwayBuilder.Api/Controllers/UserController.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using DataAccessLayer;
+using ServiceLayer.Services;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks; // For doing async (async Task<>) await
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace BroadwayBuilder.Api 
+namespace BroadwayBuilder.Api.Controllers 
 {
     public class UserController : ApiController
     {
@@ -18,5 +22,58 @@ namespace BroadwayBuilder.Api
         {
             return "Hello World! " + DateTime.Now;
         }
+
+        [Route("user/createuser")]
+        [HttpPost]
+
+        public IHttpActionResult CreateUser([FromBody] User user)
+        {
+
+            using (var dbcontext = new BroadwayBuilderContext())
+            {
+                var userService = new UserService(dbcontext);
+
+                try
+                {
+                    if (user.Username == null)
+                    {
+                        throw new Exception();
+                    }
+                    userService.CreateUser(user);
+                    dbcontext.SaveChanges();
+
+                    return Content((HttpStatusCode)201, "User Created");
+
+                }
+                // Todo: add proper error handling
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine(e.StackTrace);
+                    return Content((HttpStatusCode)400, "Must provide a Unique User Name");
+                }
+
+            }
+
+        }
+
+        //[HttpGet, Route("user/all")]
+        //public IHttpActionResult GetAllUsers()
+        //{
+        //    using (var dbcontext = new BroadwayBuilderContext())
+        //    {
+        //        UserService service = new UserService(dbcontext);
+        //        try
+        //        {
+        //            IEnumerable list = service.GetAllUsers();
+        //            return Content((HttpStatusCode)200, list);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            return Content((HttpStatusCode)500, "Oops! Something went wrong on our end");
+        //        }
+
+
+        //    }
+        //}
     }
 }
