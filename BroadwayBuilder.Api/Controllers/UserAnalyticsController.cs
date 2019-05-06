1b312fbc94b061556202a8ed4aeb24c596931c05
a-ridley <abixcastro@gmail.com>
2019-05-05 21:07:45 -0700

    added User Analysis Controller with fake data

---
+++ BroadwayBuilder.Api/Controllers/UserAnalyticsController.cs
@@ -0,0 +1,33 @@
+﻿using BroadwayBuilder.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BroadwayBuilder.Api.Controllers
+{
+    [RoutePrefix("api")]
+    public class UserAnalyticsController : ApiController
+    {
+        
+        [Route("NumberOfSuccessfulLogins")]
+        [HttpGet]
+        public IHttpActionResult NumberOfSuccessfulLogins(int month, int year)
+        {
+
+            var successfulLoginsPerMonth = new NumberOfSuccessfulLoginsResponseModel()
+            {
+                Month = month,
+                Year = year,
+                AverageSuccssfulLogins = 10,
+                MinSuccessfulLogins = 2,
+                MaxSuccessfulLogins = 15
+            };
+
+            return Ok(successfulLoginsPerMonth);
+
+        }
+    }
+}
