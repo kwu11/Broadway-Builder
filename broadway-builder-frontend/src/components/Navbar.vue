99d818d70e8d38bb28a4b46db212f8e331e1adb2
DJTai <djtai@protonmail.com>
2019-10-11 19:12:14 -0700

    Fix Navbar responsiveness
    Navbar's responsiveness to screen resizes and it's appearance on mobile view have been adjusted.

--- broadway-builder-frontend/src/components/Navbar.vue
+++ broadway-builder-frontend/src/components/Navbar.vue
@@ -1,36 +1,37 @@
 <template>
   <div>
     <!-- The entire navbar options and dropdown menu (navigation hamburger) -->
-    <v-toolbar class="hidden-sm-and-down" style="padding: 0 10em; color: white; background: linear-gradient(to right, #6F0000, #000);">
+    <v-toolbar
+      class="hidden-sm-and-down"
+      style="padding: 0 4em; color: white; background: linear-gradient(to right, #6F0000, #000);"
+    >
       <v-toolbar-title>Broadway Builder</v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- Displays the links that route to other pages -->
       <v-toolbar-items v-for="(route, index) in pageRoutes" :key="index">
-        <v-btn :to="route.link" flat  v-if="route.show">
-          <span class="color-white">
-            {{ route.title }}
-          </span>
+        <v-btn :to="route.link" flat v-if="route.show">
+          <span class="color-white">{{ route.title }}</span>
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
-    <v-toolbar class="hidden-md-and-up" style="padding: 0 0em; color: white; background: linear-gradient(to right, #6F0000, #000);">
+    <!-- Navbar for medium-sized screens and up -->
+    <v-toolbar
+      class="hidden-md-and-up"
+      style="padding: 0 0em; color: white; background: linear-gradient(to right, #6F0000, #000);"
+    >
       <v-toolbar-title>Broadway Builder</v-toolbar-title>
       <v-spacer></v-spacer>
-
       <!-- When on a smaller screen, a navigation hamburger will show up -->
       <v-menu class="hidden-md-and-up" transition="slide-y-transition" bottom left>
-        <!-- Shows the hamburger icon when on a small screen -->
         <template v-slot:activator="{ on }">
           <v-toolbar-side-icon v-on="on" right style="color: white;"></v-toolbar-side-icon>
         </template>
         <!-- Using the list of destinations, display and route to the page link -->
-        <v-list v-for="(route, index) in pageRoutes" :key="index">
-          <v-list-tile v-if="route.show">
+        <v-list v-for="(route, index) in activeRoutes" :key="index">
+          <v-list-tile>
             <v-btn :to="route.link" flat block>
-              <span class="color-black">
-                {{ route.title }}
-              </span>
+              <span class="color-black">{{ route.title }}</span>
             </v-btn>
           </v-list-tile>
         </v-list>
@@ -40,12 +41,11 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   data: function() {
-    return {
-    };
+    return {};
   },
   computed: mapState({
     pageRoutes: state => [
@@ -55,7 +55,10 @@ export default {
       { title: "Account", link: "/adminaccount/{userID}", show: state.isTheaterAdmin },
       { title: "About Us", link: "/aboutus", show: true },
       { title: "Log out", link: "/logout", show: state.token !== null }
-    ]
+    ],
+    activeRoutes() {
+      return this.pageRoutes.filter(pageRoutes => pageRoutes.show);
+    }
   })
 };
 </script>
